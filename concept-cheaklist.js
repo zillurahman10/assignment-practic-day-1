0bc6160333323bd6bf40f06a60d2be7033e2a887
zilurahman10 <raihan.brac2019@gmail.com>
2022-03-16 22:15:08 +0600

    assignment-practic-day-1

---
+++ concept-cheaklist.js
@@ -0,0 +1,108 @@
+// let
+// let iphone_13_pro_max = 120000;
+// iphone_13_pro_max = 5500
+// console.log(iphone_13_pro_max);
+
+// // const
+// const r = 57;
+// console.log(r); 
+
+
+
+
+// simple mathtematicle problem
+// addition
+var macbook = 250000;
+var asus = 100000;
+// console.log(macbook + asus);
+
+// biyoug
+var samsung = 150000;
+var iphone = 130000;
+// console.log(samsung - iphone);
+
+
+// matiplication
+var rohanAge = 13;
+var sohanAge = 11;
+// console.log(rohanAge * sohanAge);
+
+
+// divition
+var nur = 12;
+var nurLittleBrother = 4;
+// console.log(nur / nurLittleBrother);
+
+// leap Year
+function leapYear(year) {
+    if (year % 4 == 0) {
+        return true;
+    }
+    return false;
+}
+const year = 54234807234084;
+const isleapYear = leapYear(year)
+// console.log('is leap year? Ans : ', isleapYear);
+
+// conparison
+// var a = 5;
+// var g = 123;
+// if (a < g) {
+//     console.log('hoooooooooooooooooo');
+// }
+// else if (a > g) {
+//     console.log('mooooooooooooooooooooo')
+// }
+// else if (a == g) {
+//     console.log('heeeeeeeeeeeeeeeeeeee')
+// }
+// else if (a <= g) {
+//     console.log('kiiiiiiiiiiiiiiiiiiiii')
+// }
+// else if (a >= g) {
+//     console.log('jiiiiiiiiiiiiiiiiiiiiiii')
+// }
+
+
+// fullfill condition
+var avarageMark = false;
+var hardWork = true;
+if (avarageMark == true || hardWork == true) {
+    // console.log("Your Selected for SCIC")
+}
+else {
+    // console.log('jahh bhag!!!!!')
+}
+
+
+// while loop
+number = 8
+while (number <= 19) {
+    // console.log(number);
+    number = number + 1;
+}
+
+
+//array
+var number = [54, 86, 124, 95, 80, 54, 63, 20, 57]
+number[4] = 100
+number.push(56);
+number.pop();
+// console.log(number);
+
+//for
+for (i = 1; i <= 1; i++) {
+    var x = [74, 84, 29, 765, 48, 02, 63]
+    // console.log(x);
+}
+
+
+// You have an arry of numbers. display only the numbers bigger than 80
+// var y = []
+// var x = [49, 38, 38, 943, 251, 37, 29];
+for (x = 0; x > 80; x++) {
+    // if (x > 80) {
+
+    // }
+}
+
